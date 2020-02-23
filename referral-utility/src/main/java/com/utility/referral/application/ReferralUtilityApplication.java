5d09c397c9bc54540b24f7eed09b19dab654cce6
Nagbhushan Menasigi <34202071+nagzgithub@users.noreply.github.com>
2020-02-23 14:24:37 -0600

    Update ReferralUtilityApplication.java

--- referral-utility/src/main/java/com/utility/referral/application/ReferralUtilityApplication.java
+++ referral-utility/src/main/java/com/utility/referral/application/ReferralUtilityApplication.java
@@ -2,6 +2,8 @@ package com.utility.referral.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ReferralUtilityApplication {
@@ -9,5 +11,13 @@ public class ReferralUtilityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReferralUtilityApplication.class, args);
 	}
+	  public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/deleteReferral").allowedOrigins("http://localhost:3000");
+	            }
+	        };
+	    }
 
 }
